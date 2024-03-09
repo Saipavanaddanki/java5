deebf0626488f82562d2006dd98a7fb8f1dddc8b
sai <addanki.saipavan@gmail.com>
2024-03-09 21:47:20 +0530

    App is updated by logger ProjectLeader

--- Maven_Example05/src/main/java/com/nit/service/App.java
+++ Maven_Example05/src/main/java/com/nit/service/App.java
@@ -1,12 +1,21 @@
 package com.nit.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App 
-{
+{	private static Logger logger=LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
-    {
+    { logger.debug("main is started");
     	int a=30;
     	int b=20;
+    	logger.debug("a , b is initialized");
+    	logger.info("add");
+    	logger.info("sub");
+    	logger.info("mul");
     	System.out.println(add(a,b)+" "+sub(a,b)+" "+mul(a,b));
+    	
+    	logger.info("main is ended");
     	
     }
     public static int add(int i,int j) {
