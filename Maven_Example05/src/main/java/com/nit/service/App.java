91c257eb6161930161f1c997fa40491c9541491e
sai <addanki.saipavan@gmail.com>
2024-03-09 20:39:46 +0530

    update app.java by pl2

--- Maven_Example05/src/main/java/com/nit/service/App.java
+++ Maven_Example05/src/main/java/com/nit/service/App.java
@@ -1,13 +1,21 @@
 package com.nit.service;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	int a=30;
+    	int b=20;
+    	System.out.println(add(a,b)+" "+sub(a,b)+" "+mul(a,b));
+    	
+    }
+    public static int add(int i,int j) {
+    	return i+j;
+    }
+    public static int sub(int i,int j) {
+    	return i-j;
+    }
+    public static int mul(int i,int j) {
+    	return i*j;
     }
 }
